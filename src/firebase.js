db919e9e82b9eab2024cb10629688c7f816aaffe
Alex Knott <alextknott@gmail.com>
2022-06-01 20:47:07 +0100

    Create file for firebase and intialize app

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHSGCdsLHvzHovypaRsYM9BHsgjTv97RY",
+  authDomain: "chirrup-84e25.firebaseapp.com",
+  projectId: "chirrup-84e25",
+  storageBucket: "chirrup-84e25.appspot.com",
+  messagingSenderId: "376959149817",
+  appId: "1:376959149817:web:8b0ae0847fe825a9fbe428",
+  measurementId: "G-6DTGL70CWF",
+};
+
+const app = initializeApp(firebaseConfig);

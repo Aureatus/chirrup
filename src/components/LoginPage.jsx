a3e4b4be869ea3b676cf016699be409382706576
Alex Knott <alextknott@gmail.com>
2022-06-02 16:27:35 +0100

    Create 3 sections and style according to design files
    Changed double nesting of left and right sections in favour of grid

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -7,8 +7,32 @@ const Container = styled.div`
   grid-template: 95% 5% / 1fr 1fr;
 `;
 
+const LeftSection = styled.section`
+  display: flex;
+  align-items: center;
+  grid-area: 1 / 1 / 2 / 2;
+`;
+
+const RightSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 36px;
+  grid-area: 1 / 2 / 2 / 3;
+`;
+
+const Footer = styled.footer`
+  grid-area: 2 / 1 / 3 / 3;
+`;
+
 function LoginPage() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <LeftSection></LeftSection>
+      <RightSection></RightSection>
+      <Footer></Footer>
+    </Container>
+  );
 }
 
 export default LoginPage;

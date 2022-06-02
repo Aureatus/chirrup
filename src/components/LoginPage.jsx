ae1c50ef23a685d6089e34d407405769e9b7ace7
Alex Knott <alextknott@gmail.com>
2022-06-02 15:33:10 +0100

    Create styled div called Container

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,5 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template: 95% 5% / 1fr;
+`;
+
 function LoginPage() {
-  return <div></div>;
+  return <Container></Container>;
 }
 
 export default LoginPage;

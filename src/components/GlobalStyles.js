121d30ef171807b28e5891d3149ea9c806733762
Alex Knott <alextknott@gmail.com>
2022-06-02 15:40:54 +0100

    Add height and width of 100% and remove margin of body

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -2,6 +2,9 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
 body {
+    height: 100%;
+    width: 100%;
+    margin: 0%;
     background : ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
     font-family: Libre franklin;

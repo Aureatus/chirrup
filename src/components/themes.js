53db2015e050d652a107ed42df79322e2bcc7168
Alex Knott <alextknott@gmail.com>
2022-06-02 14:54:36 +0100

    Add colors for each theme

--- src/components/themes.js
+++ src/components/themes.js
@@ -1,3 +1,15 @@
-export const lightTheme = {};
+export const lightTheme = {
+  body: "#FFFFFF",
+  Color1: "#000000",
+  Color2: "#536471",
+  Color3: "#1d9bf0",
+  color4: "#f7f9f9",
+};
 
-export const darkTheme = {};
+export const darkTheme = {
+  body: "#000000",
+  Color1: "#FFFFFF",
+  Color2: "#536471",
+  Color3: "#AC9B8E",
+  color4: "#16181c",
+};

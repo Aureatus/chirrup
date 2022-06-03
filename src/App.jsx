b86dc71028775bda3668bef615e688a1ab9678a5
Alex Knott <alextknott@gmail.com>
2022-06-03 15:28:38 +0100

    Render LoginPage if user state is false

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./components/themes";
 import { GlobalStyles } from "./components/GlobalStyles";
 
+import LoginPage from "./components/LoginPage";
+
 function App() {
+  const [user, setUser] = useState(false);
   const [theme, setTheme] = useState("light");
 
   const toggleTheme = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
   };
 
-  return (
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <GlobalStyles />
-      <div></div>
-    </ThemeProvider>
-  );
+  if (!user) {
+    return (
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <GlobalStyles />
+        <LoginPage />
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;

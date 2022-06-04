f0260cdf21e22f55381ebd3a4c56df65de2259c8
Alex Knott <alextknott@gmail.com>
2022-06-04 13:42:48 +0100

    Implement first step of sign up flow

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import styled from "styled-components";
 
+import { signInWithGoogle } from "../firebaseFunctions/firebaseAuth";
+import { fetchUserName } from "../firebaseFunctions/firebaseStore";
+
 const Background = styled.div`
   height: 100%;
   width: 100%;
@@ -33,7 +36,7 @@ const Header1 = styled.div`
   font-weight: 700;
 `;
 
-const SignInWithGoogle = styled.button`
+const SignUpWithGoogle = styled.button`
   display: flex;
   width: max-content;
   align-items: center;
@@ -154,20 +157,7 @@ const SignUpPage = ({ setUser }) => {
   const [nextClicked, setNextClicked] = useState(false);
 
   if (nextClicked) {
-    return (
-      <Background>
-        <Container>
-          <SmallTwitterLogoContainer>
-            <svg viewBox="0 0 24 24">
-              <path
-                fill="rgb(29, 155, 240)"
-                d="M 23.643 4.937 c -0.835 0.37 -1.732 0.62 -2.675 0.733 c 0.962 -0.576 1.7 -1.49 2.048 -2.578 c -0.9 0.534 -1.897 0.922 -2.958 1.13 c -0.85 -0.904 -2.06 -1.47 -3.4 -1.47 c -2.572 0 -4.658 2.086 -4.658 4.66 c 0 0.364 0.042 0.718 0.12 1.06 c -3.873 -0.195 -7.304 -2.05 -9.602 -4.868 c -0.4 0.69 -0.63 1.49 -0.63 2.342 c 0 1.616 0.823 3.043 2.072 3.878 c -0.764 -0.025 -1.482 -0.234 -2.11 -0.583 v 0.06 c 0 2.257 1.605 4.14 3.737 4.568 c -0.392 0.106 -0.803 0.162 -1.227 0.162 c -0.3 0 -0.593 -0.028 -0.877 -0.082 c 0.593 1.85 2.313 3.198 4.352 3.234 c -1.595 1.25 -3.604 1.995 -5.786 1.995 c -0.376 0 -0.747 -0.022 -1.112 -0.065 c 2.062 1.323 4.51 2.093 7.14 2.093 c 8.57 0 13.255 -7.098 13.255 -13.254 c 0 -0.2 -0.005 -0.402 -0.014 -0.602 c 0.91 -0.658 1.7 -1.477 2.323 -2.41 Z"
-              ></path>
-            </svg>
-          </SmallTwitterLogoContainer>
-        </Container>
-      </Background>
-    );
+    return <div></div>;
   }
 
   return (
@@ -182,7 +172,17 @@ const SignUpPage = ({ setUser }) => {
           </svg>
         </SmallTwitterLogoContainer>
         <Header1>Sign up to Twitter</Header1>
-        <SignInWithGoogle>
+        <SignUpWithGoogle
+          onClick={async () => {
+            try {
+              const user = await signInWithGoogle();
+              const userName = await fetchUserName(user.uid);
+              setErrorMessage("Already signed up");
+            } catch (error) {
+              setNextClicked(true);
+            }
+          }}
+        >
           <svg viewBox="0 0 24 24" height={24} width={24}>
             <path
               d="M18.977 4.322L16 7.3c-1.023-.838-2.326-1.35-3.768-1.35-2.69 0-4.95 1.73-5.74 4.152l-3.44-2.635c1.656-3.387 5.134-5.705 9.18-5.705 2.605 0 4.93.977 6.745 2.56z"
@@ -202,7 +202,7 @@ const SignUpPage = ({ setUser }) => {
             ></path>
           </svg>
           <div>Sign up with Google</div>
-        </SignInWithGoogle>
+        </SignUpWithGoogle>
         <SignUpSeparator>
           <SignUpHr />
           <span>or</span>
@@ -214,7 +214,9 @@ const SignUpPage = ({ setUser }) => {
             placeholder="Email address"
             required
             onChange={(e) => {
-              setEmail(e.target.value);
+              if (e.target.validity) {
+                setEmail(e.target.value);
+              }
             }}
           />
           <PasswordInput
@@ -222,12 +224,19 @@ const SignUpPage = ({ setUser }) => {
             placeholder="password"
             required
             onChange={(e) => {
-              setPassword(e.target.value);
+              if (e.target.validity) {
+                setPassword(e.target.value);
+              }
             }}
           />
           <NextButton
             onClick={async () => {
-              setNextClicked(true);
+              if (!email || !password) {
+                setErrorMessage("Email and password must be filled in.");
+                return;
+              } else {
+                setNextClicked(true);
+              }
             }}
           >
             Next

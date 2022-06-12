f063a562068f3668b97797dddb1f38c0b69d57d0
Alex Knott <alextknott@gmail.com>
2022-06-12 14:04:59 +0100

    Implement context to App.jsx for user and userName states
    Wrap all elements in context providers and remove unnecessary props

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,13 @@ import SignInPage from "./components/LoginPage/SignInPage";
 import SignUpPage from "./components/LoginPage/SignUpPage";
 import ChooseUserName from "./components/LoginPage/ChooseUserName";
 
+import { UserContext } from "./contexts/UserContext";
+import { UserNameContext } from "./contexts/UserNameContext";
+
 function App() {
-  const [user, setUser] = useState(false);
+  const [user, setUser] = useState(null);
+  const [userName, setUserName] = useState(null);
   const [theme, setTheme] = useState("light");
-  const [userName, setUserName] = useState("");
 
   const toggleTheme = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
@@ -21,27 +24,33 @@ function App() {
 
   if (!user || (!userName && user.isAnonymous !== true)) {
     return (
-      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-        <GlobalStyles />
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" replace={true} />} />
-          <Route
-            path="/login"
-            element={<LoginPage setUser={setUser} setUserName={setUserName} />}
-          />
-          <Route
-            path="/sign-in"
-            element={<SignInPage setUser={setUser} setUserName={setUserName} />}
-          />
-          <Route path="/sign-up" element={<SignUpPage />} />
-          <Route
-            path="/choose-user-name"
-            element={
-              <ChooseUserName setUser={setUser} setUserName={setUserName} />
-            }
-          />
-        </Routes>
-      </ThemeProvider>
+      <UserContext.Provider value={{ user, setUser }}>
+        <UserNameContext.Provider value={{ userName, setUserName }}>
+          <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+            <GlobalStyles />
+            <Routes>
+              <Route
+                path="/"
+                element={<Navigate to="/login" replace={true} />}
+              />
+              <Route path="/login" element={<LoginPage />} />
+              <Route
+                path="/sign-in"
+                element={
+                  <SignInPage setUser={setUser} setUserName={setUserName} />
+                }
+              />
+              <Route path="/sign-up" element={<SignUpPage />} />
+              <Route
+                path="/choose-user-name"
+                element={
+                  <ChooseUserName setUser={setUser} setUserName={setUserName} />
+                }
+              />
+            </Routes>
+          </ThemeProvider>
+        </UserNameContext.Provider>
+      </UserContext.Provider>
     );
   }
   if (user) {

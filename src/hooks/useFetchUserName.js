00def6106cb629a5f25efd6199efb6118cb98f90
Alex Knott <alextknott@gmail.com>
2022-06-13 14:25:48 +0100

    Create custom hook that fetches userName associated with uid

---
+++ src/hooks/useFetchUserName.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import { fetchUserName } from "../firebaseFunctions/firebaseStore";
+
+const useFetchUserName = (user) => {
+  const [userName, setUserName] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const uid = user.uid;
+        console.log(uid);
+        if (uid) {
+          const currentUserName = await fetchUserName(uid);
+          setUserName(currentUserName);
+        }
+      } catch (error) {
+        setUserName(null);
+      }
+    })();
+  }, [user]);
+
+  return [userName];
+};
+
+export default useFetchUserName;

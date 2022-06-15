4259d4763e078e7bfdd445d43956820fb64a7cba
Alex Knott <alextknott@gmail.com>
2022-06-15 13:14:48 +0100

    Implement loading variable instate of useFetchUsername

--- src/hooks/useFetchUserName.js
+++ src/hooks/useFetchUserName.js
@@ -3,22 +3,26 @@ import { fetchUserName } from "../firebaseFunctions/firebaseStore";
 
 const useFetchUserName = (user) => {
   const [userName, setUserName] = useState(null);
+  const [userNameLoading, setUserNameLoading] = useState(false);
 
   useEffect(() => {
     (async () => {
       try {
         const uid = user.uid;
         if (uid) {
+          setUserNameLoading(true);
           const currentUserName = await fetchUserName(uid);
           setUserName(currentUserName);
         }
       } catch (error) {
         setUserName(null);
+      } finally {
+        setUserNameLoading(false);
       }
     })();
   }, [user]);
 
-  return [userName, setUserName];
+  return [userName, setUserName, userNameLoading];
 };
 
 export default useFetchUserName;

2ba8e8e4009c3b1f8ab62fd32320341dc8ca8975
Alex Knott <alextknott@gmail.com>
2022-06-28 17:20:15 +0100

    Improve logic of useFetchUsername to minimize unnecessary API calls and state changes

--- src/hooks/useFetchUserName.js
+++ src/hooks/useFetchUserName.js
@@ -1,25 +1,25 @@
 import { useState, useEffect } from "react";
 import { fetchUserName } from "../firebaseFunctions/firebaseStore";
 
-const useFetchUserName = (user) => {
+const useFetchUserName = (user, loading, error) => {
   const [userName, setUserName] = useState(null);
   const [userNameLoading, setUserNameLoading] = useState(true);
 
   useEffect(() => {
-    (async () => {
-      try {
-        const uid = user?.uid;
-        if (uid) {
+    if (!loading && user) {
+      (async () => {
+        try {
+          const uid = user.uid;
           const currentUserName = await fetchUserName(uid);
           setUserName(currentUserName);
+        } catch (error) {
+          console.log(error.message);
+        } finally {
+          setUserNameLoading(false);
         }
-      } catch (error) {
-        setUserName(null);
-      } finally {
-        setUserNameLoading(false);
-      }
-    })();
-  }, [user]);
+      })();
+    }
+  }, [user, loading]);
 
   return [userName, setUserName, userNameLoading];
 };

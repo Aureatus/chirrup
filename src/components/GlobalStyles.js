5bc61ce9f87ec819c3975b98b9d2306f01de6cd4
Alex Knott <alextknott@gmail.com>
2022-06-28 14:38:07 +0100

    Remove default link styling and p margin, and update body properties

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -5,8 +5,20 @@ body {
     height: 100%;
     width: 100%;
     margin: 0%;
-    background : ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.text};
+    background : ${({ theme }) => theme.primary.background};
+    color: ${({ theme }) => theme.primary.text};
     font-family: Libre franklin;
     font-size: 15px;
-}`;
+}
+* {
+    color: inherit;
+}
+a:-webkit-any-link {
+    color: black;
+    text-decoration: none;
+    color: inherit;
+}
+p {
+    margin: 0;
+}
+`;

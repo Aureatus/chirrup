8b25a3f1b0ad5fae206e108efe75ce75924d88a5
Alex Knott <alextknott@gmail.com>
2022-06-28 14:36:53 +0100

    Change themes to have multiple sets of colours with better labelling

--- src/components/themes.js
+++ src/components/themes.js
@@ -1,15 +1,45 @@
 export const lightTheme = {
-  body: "#FFFFFF", // White
-  Color1: "#000000", // Black
-  Color2: "#536471", // Gray
-  Color3: "#1d9bf0", // Blue
-  color4: "#f7f9f9", // Light gray
+  primary: {
+    background: "#FFFFFF", // White
+    text: "#000000", // Black
+    accent: "#1d9bf0", // Blue
+  },
+  secondary: {
+    background: "#f7f9f9", // Light Gray
+    text: "#000000", // Black
+    altText: "#536471", // Gray
+    accent: "#1d9bf0", // Blue
+  },
+  tertiary: {
+    background: "#0f1419", // Black
+    text: "#fff", // White
+    accent: "#1d9bf0", // Blue
+  },
+  tweet: {
+    background: "#1d9bf0", // Blue
+    text: "#FFFFFF", // White
+  },
 };
 
 export const darkTheme = {
-  body: "#000000", // Black
-  Color1: "#FFFFFF", // White
-  Color2: "#536471", // Gray
-  Color3: "#AC9B8E", // Brown
-  color4: "#16181c", // Light black
+  primary: {
+    background: "#000000", // Black
+    text: "#FFFFFF", // White
+    accent: "#FFFFFF", // White
+  },
+  secondary: {
+    background: "#16181c", // Dark gray
+    text: "#FFFFFF", // White
+    altText: "#71767b", // Medium gray
+    accent: "#FFFFFF", // White
+  },
+  tertiary: {
+    background: "#FFFFFF", // White
+    text: "#000000", // Black
+    accent: "#FFFFFF", // White
+  },
+  tweet: {
+    background: "#1d9bf0", // Blue
+    text: "#FFFFFF", // White
+  },
 };

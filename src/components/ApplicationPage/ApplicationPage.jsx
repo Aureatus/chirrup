b113151032ad2f46b3e2d47054d8f40228dd27e7
Alex Knott <alextknott@gmail.com>
2022-06-28 11:28:12 +0100

    Create ApplicationPage component that returns a container and renders children of props in it

---
+++ src/components/ApplicationPage/ApplicationPage.jsx
@@ -0,0 +1,7 @@
+import { Container } from "../ApplicationPage/StyledComponents";
+
+const ApplicationPage = (props) => {
+  return <Container>{props.children}</Container>;
+};
+
+export default ApplicationPage;

0d976bfa521b0e376b54b65fc5c0a458026b6e68
Alex Knott <alextknott@gmail.com>
2022-06-28 11:28:45 +0100

    Create styled Container component for use in ApplicationPage.jsx

---
+++ src/components/ApplicationPage/StyledComponents.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: fit-content;
+  width: 100%;
+  display: grid;
+  grid-template: 1fr / 2fr 4fr 3fr;
+  align-items: start;
+
+  box-sizing: border-box;
+  margin: 0%;
+  padding: 0px 15%;
+`;
+
+export { Container };
